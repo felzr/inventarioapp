6459e8a604a2b54f6b08e010aaf4e0676c7e49d0
felzr <felzr@outlook.com>
2020-06-19 18:16:36 -0300

    commit quarto

--- app/src/main/java/com/example/jean/inventarioApp/CadastroActivity.java
+++ app/src/main/java/com/example/jean/inventarioApp/CadastroActivity.java
@@ -12,6 +12,7 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        btnVoltar = findViewById(R.id.btn_voltar);
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/jean/inventarioApp/MainActivity.java
+++ app/src/main/java/com/example/jean/inventarioApp/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.jean.inventarioApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnNovoInventario, btnInventarios, btnRelatorio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnInventarios = findViewById(R.id.btn_iventarios);
+        btnInventarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chamaInventarios();
+            }
+        });
+        btnNovoInventario = findViewById(R.id.btn_novo_inventario);
+        btnNovoInventario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                novoInventario();
+            }
+        });
+    }
+
+    private void novoInventario() {
+        Intent intent = new Intent(this, NovoInventarioActivity.class);
+        startActivity(intent);
+    }
+
+    private void chamaInventarios() {
+        Intent intent = new Intent(this, InventarioActivity.class);
+        startActivity(intent);
     }
 }
