3670532b41e8d04dec271cb9b1213b2b6f9841b2
felzr <felzr@outlook.com>
2020-06-19 16:00:09 -0300

    commit terceiro

--- app/src/main/java/com/example/jean/inventarioApp/CadastroActivity.java
+++ app/src/main/java/com/example/jean/inventarioApp/CadastroActivity.java
@@ -3,12 +3,20 @@ package com.example.jean.inventarioApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
-
+    private Button btnVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jean/inventarioApp/LoginActivity.java
+++ app/src/main/java/com/example/jean/inventarioApp/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.jean.inventarioApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button btnLogar, btnRegsitar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogar = findViewById(R.id.btn_logar);
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logar();
+            }
+        });
+        btnRegsitar = findViewById(R.id.btn_registrar);
+        btnRegsitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrar();
+            }
+        });
+    }
+
+    private void logar() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    private void registrar() {
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
